7e80c50b51ee5dd6d217862a55e79cd2dab15b12
Alexander Lobanov <Winstondiz@gmail.com>
2016-11-06 20:35:40 +0800

    ChatChanges

--- ChatApplication/Hubs/ChatHub.cs
+++ ChatApplication/Hubs/ChatHub.cs
@@ -7,15 +7,62 @@ using System.Web;
 
 namespace ChatApplication.Hubs
 {
+	public class ChatModel
+	{
+		string personName;
+
+		public string PersonName
+		{
+			get
+			{
+				return personName;
+			}
+
+			set
+			{
+				personName = value;
+			}
+		}
+
+		public string TextBody { get; set; }
+
+
+		public bool IsMine
+		{
+			get
+			{
+				return this.personName.Equals(App.UserName);
+			}
+		}
+
+		public string ShortDate
+		{
+			get
+			{
+				return FullDate.ToString("t");
+			}
+		}
+
+		public DateTime FullDate { get; set; }
+	}
+
     public class ChatHub : Hub
     {
+		public static List<KeyValuePair<string, ChatModel>> messanges;
+
 		public object GetHistory(string room)
 		{
-			return Clients.Group(room);
+			return messanges.Where((arg) => arg.Key.Equals(room));
 		}
 		
         public void SendMessage(string name, string message, string fullDate, string roomName)
         {
+			messanges.Add(new KeyValuePair<string, ChatModel>(roomName , new ChatModel()
+			{
+				FullDate = DateTime.Parse(fullDate),
+				TextBody = message,
+				PersonName = name,
+			}));
             Clients.Group(roomName).GetMessage(name, message, fullDate);
         }
 
